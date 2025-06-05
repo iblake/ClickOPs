4bd997a1b4154652822949b4f57ae4d726bc6c9e
David Gutierrez <david.gutierrezcolodra@gmail.com>
2025-06-05 17:45:31 +0200

    a

---
+++ oe_01/oci/dev/project1/adb/dgcClickOPs2/terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "compartment_ocid" {}
+variable "region" {}
+
+
+variable "adb_password" {
+  description = "ADB password (used only if not using Vault)"
+  type        = string
+  sensitive   = true
+  default     = null
+}
+
+variable "adb_database_db_version" {
+  description = "Autonomous Database version"
+  type        = string
+  default     = "19.0.0"
+}
+
+variable "adb_database_db_name" {
+  description = "Autonomous Database name"
+  type        = string
+  default     = "dgcadb"
+}
+
+variable "adb_database_display_name" {
+  description = "Autonomous Database display name"
+  type        = string
+  default     = "dgcadb"
+}
+
+variable "adb_database_db_workload" {
+  description = "Autonomous Database workload type"
+  type        = string
+  default     = "OLTP" # Options: OLTP, DW, AJD
+}
